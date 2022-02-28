cc0fd100ec99ce9b6d9c7f891d65ddf0c5508a87
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-02-28 13:06:44 +0000

    add task

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" storage Module """
+
+import json
+from datetime import datetime 
+from models.base_model import BaseModel
+
+class FileStorage:
+    """
+        class for object storage
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self): 
+        """
+            return the dictionary object
+        """
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """
+            set new obj
+        """
+        key = "{}.{}".format(type(self).__name__, self.id)
+        FileStorage.__objects = obj
+    
+    def save(self):
+        """
+            save to the json file
+        """
+        
