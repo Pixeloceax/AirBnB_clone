d4ae7bc1b01bb6256830967be4f449a8f306ded5
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-01 14:36:33 +0000

    finish do create

--- console.py
+++ console.py
@@ -2,27 +2,69 @@
 """program called console.py that contains
 the entry point of the command interpreter"""
 import cmd
-
-from click import prompt
-from models import storage
-import json
-from models import base_model
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def emptyline(self):
-        """empty line + ENTER shouldn’t execute anything"""
+    def emptyarg(self):
+        """
+            empty line + ENTER shouldn’t execute anything
+        """
         pass
 
     def do_quit(self, arg):
-        """quit to exit the program"""
+        """
+            quit to exit the program
+        """
         return True
 
-    def do_EOF(self, line):
-        """EOF to exit the program"""
+    def do_EOF(self, arg):
+        """
+            EOF to exit the program
+        """
         return True
+
+    def do_create(self, arg):
+        """
+            create new istance of basemodel
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            instance = eval(arg)()
+            instance.save()
+            print(instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+
+    def do_show(self, arg):
+        """
+
+        """
+
+
+
+
+    def do_destroy(self, arg):
+        """
+        
+        """
+
+
+    def do_all(self, arg):
+        """
+        
+        """
+
+
+    def do_update(self, arg):    
+        """
+        
+        """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
