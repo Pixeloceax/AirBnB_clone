96fee0f4656c5e6676854476b2203c723ae85ce5
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-05 11:05:09 +0000

    add Pstyle

--- models/base_model.py
+++ models/base_model.py
@@ -31,8 +31,7 @@ class BaseModel:
                     self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today().isoformat()
-            self.updated_at = datetime.today().isoformat()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
