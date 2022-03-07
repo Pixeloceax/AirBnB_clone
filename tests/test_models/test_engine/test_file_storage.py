0f1115e1e2b5755ddfa66abc26b6b880baa57904
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-07 11:44:02 +0000

    add task

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,3 +2,33 @@
 """
   i hate test files
 """
+import unittest
+from time import sleep
+import os
+from models import base_model
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_save(self):
+        my_model = BaseModel()
+        old_updated_at = my_model.updated_at
+        sleep(0.5)
+        my_model.save()
+        self.assertEqual(my_model.updated_at, old_updated_at)
+
+    def test_to_dict(self):
+        my_model = BaseModel()
+        sleep(0.5)
+        my_model.to_dict()
+
+    def test_id(self):
+        my_model = BaseModel()
+        sleep(0.5)
+        self.id
+
+    def test_id(self):
+        my_model = BaseModel()
+        sleep(0.5)
+        self.created_at
